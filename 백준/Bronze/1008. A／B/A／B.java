83e87633d90570d82f22669da77475c27d4cff3e
jionu102 <128346722+jionu102@users.noreply.github.com>
2024-03-04 17:58:25 +0900

    [Bronze V] Title: A/B, Time: 212 ms, Memory: 17780 KB -BaekjoonHub

---
+++ 백준/Bronze/1008. A／B/A／B.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class Main{
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        double A,B;
+
+        while(true) {
+            A = scanner.nextInt(); B = scanner.nextInt();
+            if(A>0 && B<10) break;
+        }
+        System.out.println(A/B);
+        scanner.close();
+    }
+}
