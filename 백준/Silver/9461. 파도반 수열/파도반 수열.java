156388aaf880f0ce93c4b0f1aa697134427669be
jionu102 <128346722+jionu102@users.noreply.github.com>
2026-01-10 19:55:35 +0900

    [Silver III] Title: 파도반 수열, Time: 196 ms, Memory: 17932 KB -BaekjoonHub

---
+++ 백준/Silver/9461. 파도반 수열/파도반 수열.java
@@ -0,0 +1,27 @@
+import java.util.Scanner;
+
+public class Main {
+    private static final Long[] dp = new Long[101];
+
+    public static void main(String[] args) {
+        Scanner in = new  Scanner(System.in);
+        int testCases = in.nextInt();
+        in.nextLine();
+        dp[1] = dp[2] = dp[3] = 1L;
+
+        while (testCases-- > 0) {
+            int N = in.nextInt();
+            in.nextLine();
+
+            System.out.println(find(N));
+        }
+        in.close();
+    }
+
+    private static long find(int N) {
+        if (dp[N] == null) {
+            dp[N] = find(N - 2) + find(N - 3);
+        }
+        return dp[N];
+    }
+}
