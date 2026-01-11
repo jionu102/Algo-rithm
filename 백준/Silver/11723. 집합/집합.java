b18c6e94f31597e18ed435675dff35908aaafe3a
jionu102 <128346722+jionu102@users.noreply.github.com>
2026-01-11 20:25:57 +0900

    [Silver V] Title: 집합, Time: 980 ms, Memory: 313540 KB -BaekjoonHub

--- 백준/Silver/11723. 집합/집합.java
+++ 백준/Silver/11723. 집합/집합.java
@@ -1,46 +1,34 @@
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.util.List;
+import java.io.*;
 import java.util.StringTokenizer;
 
 public class Main {
     public static void main(String[] args) throws IOException {
-        int S = 0;
-
-        StringBuilder sb = new StringBuilder();
-
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        int M = Integer.parseInt(br.readLine());
+        StringBuilder sb = new StringBuilder();
+        int bits = 0;
+        int n = Integer.parseInt(br.readLine());
 
-        for (int i = 0; i < M; i++) {
+        while (n-- > 0) {
             StringTokenizer st = new StringTokenizer(br.readLine());
-            char ch = st.nextToken().charAt(0);
-            int number = -1;
+            String op = st.nextToken();
+            char c = op.charAt(0);
 
-            switch (ch) {
-                case 'a':
-                    if (st.hasMoreTokens()) {
-                        S = S | (1 << Integer.parseInt(st.nextToken()));
-                    } else {
-                        S = (1 << 21) - 2;
-                    }
-                    break;
-                case 'r' :
-                    S = S & ~(1 << Integer.parseInt(st.nextToken()));
-                    break;
-                case 'c' :
-                    int x = (S & (1 << Integer.parseInt(st.nextToken()))) != 0 ? 1 : 0;
-                    sb.append(x).append("\n");
-                    break;
-                case 't':
-                    S = S ^ (1 << Integer.parseInt(st.nextToken()));
-                    break;
-                case 'e' :
-                    S = 0;
-                    break;
+            if (c == 'a') {
+                if (op.charAt(1) == 'd') {
+                    bits |= (1 << Integer.parseInt(st.nextToken()));
+                } else {
+                    bits = (1 << 21) - 1;
+                }
+            } else if (c == 'r') {
+                bits &= ~(1 << Integer.parseInt(st.nextToken()));
+            } else if (c == 'c') {
+                sb.append((bits >> Integer.parseInt(st.nextToken())) & 1).append('\n');
+            } else if (c == 't') {
+                bits ^= (1 << Integer.parseInt(st.nextToken()));
+            } else {
+                bits = 0;
             }
         }
-        System.out.println(sb);
+        System.out.print(sb);
     }
 }
