192751f18065596f07f36c158edc9ebefc09c371
jionu102 <128346722+jionu102@users.noreply.github.com>
2026-03-23 22:56:12 +0900

    [Silver II] Title: 헌내기는 친구가 필요해, Time: 316 ms, Memory: 37084 KB -BaekjoonHub

--- 백준/Silver/21736. 헌내기는 친구가 필요해/헌내기는 친구가 필요해.java
+++ 백준/Silver/21736. 헌내기는 친구가 필요해/헌내기는 친구가 필요해.java
@@ -1,73 +1,74 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.*;
+import java.util.LinkedList;
+import java.util.Queue;
+import java.util.StringTokenizer;
 
 public class Main {
 
-    private static final StringBuilder sb = new StringBuilder();
+    private static int N;
+    private static int M;
+
+    private static final int[] dy = new int[]{1, -1, 0, 0};
+    private static final int[] dx = new int[]{0, 0, -1, 1};
 
     public static void main(String[] args) throws IOException {
         BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
 
         StringTokenizer st = new StringTokenizer(in.readLine());
-        int N = Integer.parseInt(st.nextToken());
-        int M = Integer.parseInt(st.nextToken());
+        N = Integer.parseInt(st.nextToken());
+        M = Integer.parseInt(st.nextToken());
 
+        char[][] graph = new char[N][M];
+        int iy = -1;
+        int ix = -1;
 
         for (int i = 0; i < N; i++) {
-            sb.append(in.readLine());
-        }
-
-        List<Integer>[] graph = new ArrayList[N * M];
-
-        for (int i = 0; i < N * M; i++) {
-            graph[i] = new ArrayList<>();
-
-            int[] nodes = new int[]{i - 1, i - M, i + 1, i + M};
-            boolean[] valid = new boolean[] {
-                    i % M != 0,
-                    i / M > 0,
-                    i % M < M - 1,
-                    i / M < N - 1
-            };
-            for (int j = 0; j < 4; j++) {
-                if (valid[j] && sb.charAt(nodes[j]) != 'X') {
-                    graph[i].add(nodes[j]);
-                }
+            String str = in.readLine();
+            int iIndex = str.indexOf('I');
+            if (iIndex != -1) {
+                iy = i;
+                ix = iIndex;
             }
+            graph[i] = str.toCharArray();
         }
 
-        int count = bfs(graph, sb.indexOf("I"));
-
-        if (count == 0) {
-            System.out.println("TT");
-        } else {
-            System.out.println(count);
-        }
+        int count = bfs(graph, iy, ix);
+        System.out.println(count > 0 ? count : "TT");
 
         in.close();
     }
 
-    public static int bfs(List<Integer>[] graph, int start) {
-        boolean[] visited = new boolean[graph.length];
-        visited[start] = true;
+    public static int bfs(char[][] graph, int iy, int ix) {
+        boolean[][] visited = new boolean[N][M];
+        visited[iy][ix] = true;
 
-        Queue<Integer> queue = new LinkedList<>();
-        queue.offer(start);
+        Queue<int[]> queue = new LinkedList<>();
+        queue.offer(new int[]{iy, ix});
 
         int count = 0;
 
-        while(!queue.isEmpty()) {
-            int index = queue.poll();
+        while (!queue.isEmpty()) {
+            int[] yx = queue.poll();
+            int y = yx[0];
+            int x = yx[1];
 
-            for (int num : graph[index]) {
-                if (!visited[num]) {
-                    if (sb.charAt(num) == 'P') {
+            for (int i = 0; i < 4; i++) {
+                int ny = y + dy[i];
+                int nx = x + dx[i];
+
+                if (ny < 0 || nx < 0 || ny >= N || nx >= M) {
+                    continue;
+                }
+
+                if (graph[ny][nx] != 'X' && !visited[ny][nx]) {
+                    if (graph[ny][nx] == 'P') {
                         count++;
                     }
-                    visited[num] = true;
-                    queue.offer(num);
+
+                    visited[ny][nx] = true;
+                    queue.offer(new int[]{ny, nx});
                 }
             }
         }
