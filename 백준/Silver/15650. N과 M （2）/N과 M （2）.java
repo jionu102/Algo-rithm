10e1436ae8b29129f4dce4cf297e547d69632eac
jionu102 <128346722+jionu102@users.noreply.github.com>
2026-03-23 16:47:34 +0900

    [Silver III] Title: N과 M (2), Time: 176 ms, Memory: 17752 KB -BaekjoonHub

--- 백준/Silver/15650. N과 M （2）/N과 M （2）.java
+++ 백준/Silver/15650. N과 M （2）/N과 M （2）.java
@@ -2,50 +2,39 @@ import java.util.Scanner;
 
 public class Main {
 
+    private static final StringBuilder sb = new StringBuilder();
+    private static int[] arr;
+    private static int N;
+    private static int M;
+
     public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
 
-        StringBuilder sb = new StringBuilder();
 
-        int N = in.nextInt();
-        int M = in.nextInt();
+        N = in.nextInt();
+        M = in.nextInt();
         in.nextLine();
 
-        int[] arr = new int[M];
-        int[] stack = new int[M + 1];
-        stack[0] = 1;
-        int depth = 0;
+        arr = new int[M];
 
-        while (depth >= 0) {
-            // 종료 조건
-            if (depth == M) {
-                for (int num : arr) {
-                    sb.append(num).append(' ');
-                }
-                sb.append('\n');
-                depth--;
-                stack[depth]++;
-                continue;
-            }
-
-            int i = stack[depth];
-            // 재귀에서 for 소진 → 함수 종료 대신 depth-- 로 상위로 복귀
-            if (i > N) {
-                depth--;
-                if (depth >= 0) {
-                    stack[depth]++;
-                }
-            }
-            // 재귀 로직
-            else {
-                arr[depth] = i;
-                depth++;
-                stack[depth] = i + 1;
-            }
-        }
-
+        dfs(1, 0);
         System.out.println(sb);
 
         in.close();
+    }
+
+    public static void dfs(int start, int depth) {
+        if (depth >= M) {
+            for (int num : arr) {
+                sb.append(num).append(' ');
+            }
+            sb.append('\n');
+            return;
+        }
+
+        for (int i = start; i <= N; i++) {
+            arr[depth] = i;
+            dfs(i + 1, depth + 1);
+        }
     }
 }
