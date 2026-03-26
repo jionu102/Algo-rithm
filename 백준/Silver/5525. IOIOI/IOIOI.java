a8b51c058b186478fbeae0b93bccd37272535ce2
jionu102 <128346722+jionu102@users.noreply.github.com>
2026-03-26 20:11:25 +0900

    [Silver I] Title: IOIOI, Time: 124 ms, Memory: 14724 KB, Score: 50 point -BaekjoonHub

--- 백준/Silver/5525. IOIOI/IOIOI.java
+++ 백준/Silver/5525. IOIOI/IOIOI.java
@@ -1,16 +1,16 @@
-import java.util.Scanner;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 
 public class Main {
 
-    public static void main(String[] args) {
-        Scanner in = new Scanner(System.in);
+    public static void main(String[] args) throws IOException {
+        BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
 
-        int N = in.nextInt();
-        in.nextLine();
-        int M = in.nextInt();
-        in.nextLine();
+        int N = Integer.parseInt(in.readLine());
+        int M = Integer.parseInt(in.readLine());
 
-        String str = in.nextLine();
+        String str = in.readLine();
         StringBuilder patternStr = new StringBuilder();
         patternStr.append("IOI");
         for (int i = 1; i < N; i++) {
