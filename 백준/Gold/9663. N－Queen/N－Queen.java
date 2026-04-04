c6446477ba4eed053fda5e0dc6a0698aa6a835bf
jionu102 <128346722+jionu102@users.noreply.github.com>
2026-04-05 00:20:33 +0900

    [Gold IV] Title: N-Queen, Time: 5556 ms, Memory: 17860 KB -BaekjoonHub

--- 백준/Gold/9663. N－Queen/N－Queen.java
+++ 백준/Gold/9663. N－Queen/N－Queen.java
@@ -2,50 +2,50 @@ import java.util.Scanner;
 
 public class Main {
 
-    private static int N;
-    private static int count = 0;
     private static int[] arr;
+    private static int count = 0;
 
     public static void main(String[] args) {
-        Scanner in= new Scanner(System.in);
+        Scanner in = new Scanner(System.in);
 
-        N = in.nextInt();
+        int N = in.nextInt();
         in.nextLine();
 
         arr = new int[N];
-
-        backTracking(0);
+        int depth = 0;
+        backTracking(N, depth);
 
         System.out.println(count);
 
         in.close();
     }
 
-    private static void backTracking(int depth) {
+    private static void backTracking(int N, int depth) {
         if (depth == N) {
-            count ++;
+            count++;
             return;
         }
 
         for (int i = 0; i < N; i++) {
             arr[depth] = i;
 
-            if (possibility(depth)) {
-                backTracking(depth + 1);
+            if (isPossible(depth)) {
+                backTracking(N, depth + 1);
             }
         }
     }
 
-    private static boolean possibility(int depth) {
+    private static boolean isPossible(int depth) {
         for (int i = 0; i < depth; i++) {
             if (arr[i] == arr[depth]) {
                 return false;
             }
 
-            if (Math.abs(arr[depth] - arr[i]) == depth - i) {
+            if (depth - i == Math.abs(arr[depth] - arr[i])) {
                 return false;
             }
         }
+
         return true;
     }
 }
