7b6d7631e4792ff9ef10326d62634f687c9f7a66
jionu102 <128346722+jionu102@users.noreply.github.com>
2026-06-07 00:31:54 +0900

    [level 2] Title: 바이러스 파이프, Time: 4.42 ms, Memory: 68.4 MB -BaekjoonHub

--- 프로그래머스/2/468373. 바이러스 파이프/바이러스 파이프.java
+++ 프로그래머스/2/468373. 바이러스 파이프/바이러스 파이프.java
@@ -1,72 +1,101 @@
+import java.util.*;
+
 class Solution {
     
+    List<Integer>[] pipeA;
+    List<Integer>[] pipeB;
+    List<Integer>[] pipeC;
+    
     public int solution(int n, int infection, int[][] edges, int k) {
-        int[] infections = new int[n + 1];
-        infections[infection]++;
-        int startA = find(1, infections, edges, k, 0);
-        int startB = find(2, infections, edges, k, 0);
-        int startC = find(3, infections, edges, k, 0);
+        pipeA = new ArrayList[n + 1];
+        pipeB = new ArrayList[n + 1];
+        pipeC = new ArrayList[n + 1];
+        buildPipe(1, pipeA, edges);
+        buildPipe(2, pipeB, edges);
+        buildPipe(3, pipeC, edges);
         
-        return Math.max(startA, Math.max(startB, startC));
+        boolean[] infections = new boolean[n + 1];
+        infections[infection] = true;
+    
+        return dfs(infections, k, 0);
     }
     
-    private int find(int type, int[] infections, int[][] edges, int k, int depth) {
+    private int dfs(boolean[] infections, int k, int depth) {
         if (depth == k) {
-            return biggerZeroSize(infections);
+            return isTrueSize(infections);
         }
-        
-        int[] memo = new int[infections.length];
-        
-        boolean changed;
 
-        do {
-            changed = false;
-
-            for (int[] edge : edges) {
-                if (edge[2] != type) continue;
-
-                int a = edge[0];
-                int b = edge[1];
-
-                if (infections[a] > 0 && infections[b] == 0) {
-                    infections[b]++;
-                    memo[b]++;
-                    changed = true;
-                }
-
-                if (infections[b] > 0 && infections[a] == 0) {
-                    infections[a]++;
-                    memo[a]++;
-                    changed = true;
-                }
-            }
-        } while (changed);
-        
-        // A 선택
-        int resultA = find(1, infections, edges, k, depth + 1);
-        // B 선택
-        int resultB = find(2, infections, edges, k, depth + 1);
-        // C 선택
-        int resultC = find(3, infections, edges, k, depth + 1);
-        
-        rollback(infections, memo);
+        int resultA = openPipe(infections, pipeA, k, depth);
+        int resultB = openPipe(infections, pipeB, k, depth);
+        int resultC = openPipe(infections, pipeC, k, depth);
         
         return Math.max(resultA, Math.max(resultB, resultC));
     }
     
-    private int biggerZeroSize(int[] infections) {
-        int count = 0;
+    private int openPipe(boolean[] infections, List<Integer>[] pipe, int k, int depth) {
+        List<Integer> memo = bfs(infections, pipe);
         
-        for (int infection : infections) {
-            if (infection > 0) count++;
+        int result = dfs(infections, k, depth + 1);
+        
+        rollback(infections, memo);
+        
+        return result;
+    }
+    
+    private int isTrueSize(boolean[] infections) {
+        int count = 0;
+        for (boolean infection : infections) {
+         if (infection) count++;
         }
         
         return count;
     }
     
-    private void rollback(int[] infection, int[] memo) {
-        for (int i = 0; i < memo.length; i++) {
-            infection[i] -= memo[i];
+    private void rollback(boolean[] infections, List<Integer> memo) {
+        for (int m : memo) {
+            infections[m] = false;
+        }
+    }
+    
+    private List<Integer> bfs(boolean[] infections, List<Integer>[] pipe) {
+        Queue<Integer> queue = new ArrayDeque<>();
+        for (int i = 1; i < pipe.length; i++) {
+            if (infections[i] && !pipe[i].isEmpty()) {
+                queue.offer(i);
+            }
+        }
+        
+        List<Integer> memo = new ArrayList<>();
+            
+        while(!queue.isEmpty()) {
+            int root = queue.poll();
+            for (int node : pipe[root]) {
+                // infections 가 visited 역할 수행
+                if (infections[root] && !infections[node]) {
+                    infections[node] = true;
+                    queue.offer(node);
+                    memo.add(node);
+                }
+            }
+        }
+        
+        return memo;
+    }
+    
+    private void buildPipe(int type, List<Integer>[] pipe, int[][] edges) {
+        initPipe(pipe);
+        
+        for(int[] edge : edges) {
+            if (edge[2] == type) {
+                pipe[edge[0]].add(edge[1]);
+                pipe[edge[1]].add(edge[0]);
+            }
+        }
+    }
+    
+    private void initPipe(List<Integer>[] pipe) {
+        for(int i = 0; i < pipe.length; i++) {
+            pipe[i] = new ArrayList<>();
         }
     }
 }
